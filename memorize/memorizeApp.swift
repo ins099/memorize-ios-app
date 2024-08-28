675453b429c540f8cba60432fd8ec446d315a05f
Insaram Alam Siddique <alaminsaram92@gmail.com>
2024-08-28 18:55:43 +0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Insaram Alam Siddique on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
